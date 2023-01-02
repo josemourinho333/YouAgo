40b11bf3f7450ba87d8b69470f71595d8ab5ac62
philyoo <philyoo@philyoos-MacBook-Pro.local>
2023-01-02 13:05:09 -0800

    Config firebase and create context

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,11 @@
+import { AuthProvider } from '../context/AuthContext';
 import '../styles/globals.css';
 
 export default function App({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
-
-  return getLayout(<Component {...pageProps} />)
+  return getLayout(
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  )
 }
