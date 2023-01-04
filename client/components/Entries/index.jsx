d7c9beb12be3533e72519165a811b1540287f34c
philyoo <philyoo@philyoos-MacBook-Pro.local>
2023-01-04 00:03:41 -0800

    Add read only for submitted entries

--- client/components/Entries/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const EntriesIndex = () => {
-  return (
-    <div>index fsdfsd</div>
-  )
-}
-
-export default EntriesIndex;
--- client/pages/journal/entries/[date].jsx
+++ client/pages/journal/entries/[date].jsx
@@ -3,6 +3,7 @@ import Router from 'next/router';
 import { useRouter } from 'next/router';
 import Layout from '../../../components/Dashboard/layout';
 import { useAuth } from '../../../context/AuthContext';
+import moment from 'moment';
 
 const Entry = () => {
   const router = useRouter();
@@ -16,7 +17,6 @@ const Entry = () => {
     }
 
     const currentFocus = journalEntries.filter((entry) => {
-      console.log('er', entry.date === date);
       return entry.date === date;
     });
 
@@ -24,8 +24,35 @@ const Entry = () => {
 
   }, [date, currentUser, journalEntries]);
 
+  const mappedGrateful = focusEntry?.grateful?.map((grate, index) => {
+    return (
+      <div key={index} className="text-neutral-content w-full max-w-full font-light text-xl lg:text-2xl">
+        {grate}
+      </div>
+    )
+  })
+
   return (
-    <div>Entry: {date} | {focusEntry.diary}</div>
+    <div className="h-full flex flex-col items-start p-5 gap-y-5">
+      <div className="flex flex-row justify-between items-center w-full">
+        <h1 className="text-neutral font-bold text-4xl lg:text-6xl">{moment(focusEntry?.date).format('ll')}</h1>
+      </div>
+      <div className="flex flex-col h-full w-full gap-5 lg:flex-row">
+        <div className="p-2 font-light basis-8/12 min-h-full text-neutral-content text-xl lg:text-2xl">{focusEntry?.diary}</div>
+        <div className="flex flex-col basis-4/12 p-2 gap-y-5">
+          <div className="flex-1 flex flex-col min-h-[50vh] items-center justify-center">
+            <div className="rounded-lg bg-neutral border-none w-full h-full text-neutral-content font-semibold text-3xl lg:text-5xl normal-case text-center flex justify-center items-center">
+              You felt<br/>
+              {focusEntry?.mood}
+            </div>
+          </div>
+          <div className="flex flex-col gap-y-5">
+            <div className="label-text text-xl lg:text-2xl font-light text-neutral">I&rsquo;m grateful for...</div>
+            {mappedGrateful}
+          </div>
+        </div>  
+      </div>
+    </div>
   )
 }
 
