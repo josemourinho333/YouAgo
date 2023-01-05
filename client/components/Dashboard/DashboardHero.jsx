4552a9c95f08b2889f997633fe17b6d036b4c4ca
philyoo <philyoo@philyoos-MacBook-Pro.local>
2023-01-04 19:40:06 -0800

    Bring currentUser from context and dispaly name on dashboard

--- client/components/Dashboard/DashboardHero.jsx
+++ client/components/Dashboard/DashboardHero.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useAuth } from '../../context/AuthContext';
 
 const DashboardHero = () => {
+  const { currentUser } = useAuth();
+
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content text-center">
         <div className="max-w-xl">
-          <h1 className="text-5xl font-bold">Welcome Back, [name]</h1>
+          <h1 className="text-5xl font-bold">Welcome Back, {currentUser.displayName.split(' ')[0]}</h1>
           <p className="py-6">Dashboard main page is not yet ready. Come back and check for updates!</p>
           <button className="btn btn-primary">Journal Today</button>
         </div>
